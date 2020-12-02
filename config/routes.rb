31e5d4b1e5aeacc326332a7a6efd1528ab115793
EC2 Default User <ootakaebisu@gmail.com>
2020-12-02 06:57:14 +0000

    [fix]ルーティングの記述を整える

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,28 @@
 Rails.application.routes.draw do
-  
+
   namespace :admins do
+    get 'homes/top'
     resources :order_details, only:[:update]
     resources :orders, only:[:index, :show, :update]
     resources :customers, only:[:index, :show, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
   end
-  
-  namespace :customers do
-    get 'orders/confirm'
-    get 'orders/complete'
-    resources :orders, only:[:new, :create, :index, :show]
-    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
-  end
-
-
-
-  namespace :customers do
-    resources :items, only:[:index, :show]
-
-    resources :customers, only:[:edit, :update]
-    get 'customers/my_page/:id' => 'customers#show', as: 'my_page'
-    get 'customers/unsubscribe'
-    patch 'customers/withdraw'
-
-    resources :cart_items, only:[:index, :update, :destroy, :create]
-    delete 'cart_items/destroy_all'
-
-  end
-
-
-  namespace :admins do
-    get 'homes/top'
-  end
 
   namespace :customers do
     get 'homes/top'
     get 'homes/about'
+    get 'orders/confirm'
+    get 'orders/complete'
+    resources :orders, only:[:new, :create, :index, :show]
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+    resources :items, only:[:index, :show]
+    get 'customers/my_page/:id' => 'customers#show', as: 'my_page'
+    get 'customers/unsubscribe'
+    patch 'customers/withdraw'
+    resources :customers, only:[:edit, :update]
+    delete 'cart_items/destroy_all'
+    resources :cart_items, only:[:index, :update, :destroy, :create]
   end
 
   devise_for :admins, controllers: {
