261452f57174e75ed524aab7bf251441d10821a7
EC2 Default User <ootakaebisu@gmail.com>
2020-12-03 18:09:41 +0000

    [add]ログインのviewを整える/他微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_customer!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
-  
-
   protected
-  
-  # def after_sign_in_path_for(resource)
-  #    root_path(resource)
-  ## end
 
-  def after_sign_out_path_for(resource)
-    root_path
-  end
+    def after_sign_in_path_for(resource)
+        root_path(resource)
+    end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
-  end
+    def after_sign_out_path_for(resource)
+      root_path
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+    end
 end
-
--- app/controllers/customers/homes_controller.rb
+++ app/controllers/customers/homes_controller.rb
@@ -2,13 +2,9 @@ class Customers::HomesController < ApplicationController
 
   def top
     @genres = Genre.all
-    # @items = Item.order("RAND()").limit(4)
-    # ↑の代わりに仮置き(データがないため)↓
-    @items = Item.all
+    @items = Item.order("RAND()").limit(4)
   end
 
   def about
   end
 end
-
-
