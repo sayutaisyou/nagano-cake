93add1f9157c73b055e9f2fa9bd8d4cefca59997
EC2 Default User <ec2-user@ip-172-31-37-182.ec2.internal>
2020-12-03 11:58:23 +0000

    [add]顧客編集画面

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -8,8 +8,21 @@ class Admins::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    if customer.update
+      redirect_to admins_customer_path(customer.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def customer_paramas
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
