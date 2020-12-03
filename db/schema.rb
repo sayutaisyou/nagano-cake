23dbbeea242fffbe5e7913fd10122deec43083c8
EC2 Default User <ec2-user@ip-172-31-37-182.ec2.internal>
2020-12-03 04:09:18 +0000

    [add]会員一覧.会員詳細のview

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_090014) do
+ActiveRecord::Schema.define(version: 2020_12_02_122429) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_090014) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
-    t.boolean "is_deleted", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
