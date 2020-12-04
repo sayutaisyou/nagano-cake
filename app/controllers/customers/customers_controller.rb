a56ff4309e2f1526f1f9c3520202355406c09a0f
EC2 Default User <ootakaebisu@gmail.com>
2020-12-04 05:23:44 +0000

    [add]顧客の登録情報編集画面の追加/他微調整

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -3,9 +3,17 @@ class Customers::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "編集内容を保存しました！"
+      redirect_to customers_my_page_path(@customer)
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
@@ -13,4 +21,10 @@ class Customers::CustomersController < ApplicationController
 
   def withdraw
   end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+    end
+
 end
