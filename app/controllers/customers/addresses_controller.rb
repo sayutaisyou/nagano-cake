3ff38eee285cd51a5813352765dea9c0391f3c92
EC2 Default User <ec2-user@ip-172-31-46-150.ec2.internal>
2020-12-04 06:04:57 +0000

    [Fix]配送先機能の修正

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,10 +1,10 @@
 class Customers::AddressesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_customer!
   
   def index
     @address = Address.new
-    @address.customer_id = current_user.id
-    @addresses = current_user.Address.all
+    @address.customer_id = current_customer.id
+    @addresses = current_customer.addresses.all
   end
 
   def edit
@@ -12,6 +12,8 @@ class Customers::AddressesController < ApplicationController
   end
   
   def create
+    @address = current_customer.addresses.new(address_params)
+    @addresses = current_customer.addresses.all
     if @address.save
       redirect_to customers_addresses_path, notice: "You have created address successfully."
     else
@@ -29,6 +31,7 @@ class Customers::AddressesController < ApplicationController
   end
   
   def destroy
+    @address = Address.find(params[:id])
     @address.destroy
     redirect_to customers_addresses_path
   end
@@ -36,6 +39,6 @@ class Customers::AddressesController < ApplicationController
   
   protected
   def address_params
-    params.require(:address).permit(:posta_code, :address, :name)
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,20 +1,20 @@
 class Customers::OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_customer!
   
   def new
     @order = Order.new
-    @order.customer_id = current_user.id
+    @order.customer_id = current_customer.id
     @address = Address.find(params[:id])
-    @address.customer_id = current_user.id
+    @address.customer_id = current_customer.id
   end
   
   def confirm
     if radio_button = "0"
-      address = current_user.customers.address
+      address = current_customer.customers.address
     elsif radio_button = "1"
-      address = current_user.customers.address.all
+      address = current_customer.customers.address.all
     else
-      address = current_user.addresses.address(params[:id])
+      address = current_customer.addresses.address(params[:id])
     end
     address = Address.find(params[:id])
   end
