51a3874972dda9b412725425df42e3deb6a3b51b
EC2 Default User <ec2-user@ip-172-31-46-150.ec2.internal>
2020-12-09 05:51:54 +0000

    delete css file

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,12 +1,6 @@
 class Customers::AddressesController < ApplicationController
   before_action :authenticate_customer!
-  before_action :hoge, if: proc { current_customer.is_deleted == true }
-
-  def hoge
-    sign_out
-    redirect_to new_customer_registration_path
-  end
-
+  
   def index
     @address = Address.new
     @address.customer_id = current_customer.id
